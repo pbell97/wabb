cce09411ec6dd9e9dca25c7b3c4e8c877969a23f
Kohler Smallwood <30441642+Smallwoody@users.noreply.github.com>
2019-10-31 08:27:21 -0500

    Seemingly working google Auth
    I think that this is working for google Auth. My computer isnt able to test it well, but it is running on our client ID, and pulls up Google Play for sign in. Also returns values from the login to object SignInResultCallback

---
+++ wabb/wabb/SigninQuickstart/obj/Debug/81/android/src/com/xamarin/signinquickstart/MainActivity.java
@@ -0,0 +1,103 @@
+package com.xamarin.signinquickstart;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		android.view.View.OnClickListener,
+		com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onStart:()V:GetOnStartHandler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onActivityResult:(IILandroid/content/Intent;)V:GetOnActivityResult_IILandroid_content_Intent_Handler\n" +
+			"n_onStop:()V:GetOnStopHandler\n" +
+			"n_onClick:(Landroid/view/View;)V:GetOnClick_Landroid_view_View_Handler:Android.Views.View/IOnClickListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onConnectionFailed:(Lcom/google/android/gms/common/ConnectionResult;)V:GetOnConnectionFailed_Lcom_google_android_gms_common_ConnectionResult_Handler:Android.Gms.Common.Apis.GoogleApiClient/IOnConnectionFailedListenerInvoker, Xamarin.GooglePlayServices.Base\n" +
+			"";
+		mono.android.Runtime.register ("SigninQuickstart.MainActivity, SigninQuickstart", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("SigninQuickstart.MainActivity, SigninQuickstart", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onStart ()
+	{
+		n_onStart ();
+	}
+
+	private native void n_onStart ();
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onActivityResult (int p0, int p1, android.content.Intent p2)
+	{
+		n_onActivityResult (p0, p1, p2);
+	}
+
+	private native void n_onActivityResult (int p0, int p1, android.content.Intent p2);
+
+
+	public void onStop ()
+	{
+		n_onStop ();
+	}
+
+	private native void n_onStop ();
+
+
+	public void onClick (android.view.View p0)
+	{
+		n_onClick (p0);
+	}
+
+	private native void n_onClick (android.view.View p0);
+
+
+	public void onConnectionFailed (com.google.android.gms.common.ConnectionResult p0)
+	{
+		n_onConnectionFailed (p0);
+	}
+
+	private native void n_onConnectionFailed (com.google.android.gms.common.ConnectionResult p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ wabb/wabb/SigninQuickstart/obj/Debug/81/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 59115c83d70b53024eae658d9bd70fef1af35b24 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1023
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 268027
+	.include	"typemap.mj.inc"
